cb39e6c2de320f642b6f3ee2e1a76cdabd1e3c5f
BogdanDrumesi10 <bogdandrumesi@gmail.com>
2019-05-25 23:14:46 +0300

    Added insert any data/delete project

--- utils.js
+++ utils.js
@@ -6,11 +6,12 @@ const db = new sqlite3.Database(`./database/sqlite3.db`, sqlite3.OPEN_READWRITE 
         return console.error(err.message);
     }
     console.log(`Connected to the in-memory SQlite database.`);
+    createTableIfNotExists();
 });
 
 exports.db = db;
 
-exports.createTableIfNotExists = () => {
+function createTableIfNotExists() {
     // CREATE DATABASE TABLE
     db.run('CREATE TABLE IF NOT EXISTS it_projects (' +
         'project_id INTEGER PRIMARY KEY AUTOINCREMENT,' +
@@ -22,17 +23,17 @@ exports.createTableIfNotExists = () => {
         'created_by TEXT NOT NULL,' +
         'modified_on INTEGER NOT NULL,' +
         'modified_by TEXT NOT NULL)');
-};
+}
 
 exports.insertDummyData = () => {
     let dummyData = {
-        project_name: "Test",
-        start_date: 1,
-        target_end_date: 2,
-        actual_end_date: 3,
-        created_on: new Date(),
+        project_name: "Test Project Name",
+        start_date: new Date().getTime(),
+        target_end_date: new Date().getTime(),
+        actual_end_date: new Date().getTime(),
+        created_on: new Date().getTime(),
         created_by: "Bogdan",
-        modified_on: new Date(),
+        modified_on: new Date().getTime(),
         modified_by: "Bogdan modifify"
     };
 
@@ -41,6 +42,25 @@ exports.insertDummyData = () => {
             ('${dummyData.project_name}', '${dummyData.start_date}', '${dummyData.target_end_date}',
             '${dummyData.actual_end_date}', '${dummyData.created_on}', '${dummyData.created_by}',
             '${dummyData.modified_on}', '${dummyData.modified_by}')`);
+};
+
+exports.insertData = (project_name, start_date, target_end_date, actual_end_date,
+                      created_on, created_by, modified_on, modified_by) => {
+    db.run(`INSERT INTO it_projects(project_name, start_date, target_end_date, actual_end_date,
+            created_on, created_by, modified_on, modified_by) VALUES
+            ('${project_name}', '${start_date}', '${target_end_date}',
+            '${actual_end_date}', '${created_on}', '${created_by}',
+            '${modified_on}', '${modified_by}')`);
+};
+
+exports.deleteProject = (project_name) => {
+    db.run(`DELETE FROM it_projects WHERE project_name LIKE '${project_name}'`, (err) => {
+        if (err) {
+            console.log(`Error deleting project "${project_name}" ${err}`);
+        } else {
+            console.log(`Successfully deleted project "${project_name}"`);
+        }
+    })
 };
 
 exports.printDatabase = () => {
